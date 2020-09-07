ff20364dd9cd8098e978cbcf653045c1149e5513
YashNin <yashnindane@gmail.com>
2020-09-07 20:58:10 +0530

    Initial commit

---
+++ Assets/Scripts/MenuUIController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIController : MonoBehaviour
+{
+    public void SelectCharacter(Transform selectedCharacter)
+    {
+        GameConstants.gameConstants.selectedCharacter = selectedCharacter.name;
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerConfig playerConfig;
+
+    public CharacterController cc;
+
+    float walkSpeed;
+    float rotateSpeed;
+    
+    public Transform cam;
+    
+    float ver;
+    
+    public Transform myEmote;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+
+        cc = GetComponent<CharacterController>();
+  
+        cam = Camera.main.transform;
+        //cam.parent = transform;
+
+        walkSpeed = playerConfig.walkSpeed;
+        rotateSpeed = playerConfig.rotateSpeed;
+
+        UIController.uIController.myPlayer = this;
+
+        myEmote = transform.GetChild(0);
+    }
+    
+    void Update()
+    {
+        ver = Input.GetAxisRaw("Vertical");
+        
+        transform.Rotate(0, Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime, 0);
+
+        var forwardVector = transform.forward.normalized * walkSpeed * Time.deltaTime * ver;
+        
+        cc.Move(forwardVector);
+
+
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            UIController.uIController.ToggleEmoteMenu();
+        }
+
+        var camPos = (transform.forward.normalized * - playerConfig.cameraDistance) + transform.position;
+        camPos.y = playerConfig.cameraHeight;
+        cam.position = camPos;
+        cam.eulerAngles += new Vector3(playerConfig.cameraTiltSpeed * -Input.GetAxis("Mouse Y") * Time.deltaTime, 0, 0);
+        cam.transform.eulerAngles = new Vector3(Mathf.Clamp(cam.transform.eulerAngles.x, playerConfig.cameraTiltRange.x, playerConfig.cameraTiltRange.y), transform.eulerAngles.y, 0);
+    }
+
+}
